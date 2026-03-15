df0bf928b15b7e1b6644b20d3ff72ab8e7692fa1
januuu02 <128676707+januuu02@users.noreply.github.com>
2026-03-15 16:16:38 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
 
-const dbUser = "admin";
-const dbPassword = "password123";
+// XSS Vulnerable endpoint
+app.get("/search", (req, res) => {
+    const query = req.query.q;
+    res.send("<h1>Search results for: " + query + "</h1>");
+});
 
 app.get("/", (req, res) => {
-    res.send("Backend server running");
+    res.send("Backend running");
 });
 
 app.listen(3000, () => {
