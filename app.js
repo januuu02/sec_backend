6bda5e0e4537e60d4572debae6a0b903d2aee8c4
januuu02 <128676707+januuu02@users.noreply.github.com>
2026-03-15 16:09:02 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
 
+const dbUser = "admin";
+const dbPassword = "password123";
+
 app.get("/", (req, res) => {
-  res.send("Backend running");
+    res.send("Backend server running");
 });
 
 app.listen(3000, () => {
-  console.log("Server running on port 3000");
+    console.log("Server running on port 3000");
 });
